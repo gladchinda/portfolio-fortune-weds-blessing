2fae5ec55f2327b2192cc64f936bdfa3d66599a3
gladx <gladxeqs@gmail.com>
2017-02-09 22:36:00 +0100

    Initial homepage commit

---
+++ resources/views/app/layout.blade.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Fortune-weds-Blessing</title>
+        <meta name="description" content="Wedding arrangements for the wedding between Fortune Chinda and Blessing Peter in March 2017.">
+        <meta name="keywords" content="Fortune, Blessing, wedding, weds, fortunewedsblessing">
+        <meta name="author" content="Glad Chinda <gladxeqs@gmail.com>">
+        
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="/favicon.ico" type="image/x-icon">
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="/assets/css/vendor.css">
+        <link rel="stylesheet" href="/assets/css/app.css">
+
+        <!-- Mordernizr.js -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+
+        @section('head-metadata')
+        @show
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+        <!-- Google Analytics -->
+        <!-- <script src="https://www.google-analytics.com/analytics.js" async defer></script> -->
+    </head>
+    <body ng-app="main-app">
+        <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+        <!-- <script>
+            window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+            ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+        </script> -->
+
+        @section('content')
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        @show
+
+        @section('deferred-scripts')
+        <script src="/assets/js/libs.js"></script>
+        <script src="/assets/js/app.js"></script>
+        @show
+    </body>
+</html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,35 +1,56 @@
-<!doctype html>
-<html class="no-js" lang="">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Fortune-weds-Blessing</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+@extends('app.layout')
 
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
-        <!--<link rel="stylesheet" href="css/main.css"> -->
-    </head>
-    <body>
-        <!--[if lt IE 8]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
+@section('content')
+    @parent
+    
+    <!-- begin navbar -->
+    <nav id="global-nav" class="navbar navbar-default" role="navigation">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#global-nav-collapsible" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">
+                    <img src="http://localhost/www/zynes/eway/ui/app/images/139426.png" alt="fortune-weds-blessing">
+                    <span class="hidden-xs hidden-sm hidden-md brand-label">fortunewedsblessing</span>
+                </a>
+            </div>
+            <div id="global-nav-collapsible" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">The Bride</a></li>
+                            <li><a href="#">The Groom</a></li>
+                            <li><a href="#">Our Story</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Feeds</a></li>
+                    <li><a href="#">Events</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- end navbar -->
 
-        <!-- Add your site or application content here -->
-        <!-- <p>Hello world! This is HTML5 Boilerplate.</p> -->
+    <!-- begin hero -->
+    <div id="cover-hero" class="hero" role="banner">
+        <div class="hero-container">
+            <div class="hero-banner">
+                <h1>Fortune weds Blessing</h1>
+                <a href="#" class="btn btn-lg btn-primary hero-action-btn">Event Schedules</a>
+            </div>
+        </div>
+    </div>
+    <!-- end hero -->
 
-        <script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.4.min.js"><\/script>')</script>
-
-        <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
-        <script>
-            window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
-            ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
-        </script>
-        <script src="https://www.google-analytics.com/analytics.js" async defer></script>
-    </body>
-</html>
+    <div class="container-fluid" style="min-height:800px;">
+        <div class="row" style="padding:0 200px;">
+            <div class="col-md-6 col-xs-12">Hello</div>
+            <div class="col-md-6 col-xs-12">There</div>
+        </div>
+    </div>
+@endsection
